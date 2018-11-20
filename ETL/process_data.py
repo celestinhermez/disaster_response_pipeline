451714c41350f36fb26beb1b9b79d2979ad951e1
Celestin Hermez <celestinhermez@gmail.com>
2018-11-20 15:52:55 -0800

    Load and clean the data
    Loading the two datasets into dataframes with the help of Pandas,
    cleaning the data with Pandas:
    - making sure that categories are columns with binary values
    - removing duplicates

--- ETL/process_data.py
+++ ETL/process_data.py
@@ -1,13 +1,58 @@
 import sys
-
+import pandas as pd
+from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    '''
+    A function to load our two datasets, returning a merged dataframe
 
+    :param messages_filepath: a string with the filepath to the file with the messages. This file has to be a CSV,
+    and to be in the same folder as our script
+    :param categories_filepath: a string with the filepath to the file with the categories of each message.
+    This file has to be a CSV, and to be in the same folder as our script
+
+    :return: tw
+    '''
+
+    messages = pd.read_csv('messages.csv')
+    categories = pd.read_csv('categories.csv')
+
+    df = messages.merge(categories, on ='id', how='inner')
+
+    return df
 
 def clean_data(df):
-    pass
+    '''
+    A function to clean our merged dataframe.
 
+    :param df: a merged dataframe of messages and the associated categories
+    :return: the same dataframe, cleaned
+    '''
+
+    # We isolate the categories from the categories column
+    categories = df.categories.str.split(';', expand=True)
+
+    # We use the first row for column names
+    row = categories.iloc[0]
+    category_colnames = row.apply(lambda x: x[:-2])
+    categories.columns = category_colnames
+
+    # We only keep the 0 and 1 values
+    for column in categories:
+        categories[column] = categories[column].str[-1]
+        categories[column] = categories[column].astype(int)
+
+    # We replace the categories column from our original dataframe with these new columns
+    df.drop('categories', axis=1, inplace=True)
+    df = pd.concat([df, categories], axis=1)
+
+    # We finish our cleaning by removing duplicates
+    # and rows where the "related" category is 2 (values should only be 0 and 1
+
+    df.drop_duplicates(inplace=True)
+    df = df.loc[df.related != 2,:]
+
+    return(df)
 
 def save_data(df, database_filename):
     pass  
